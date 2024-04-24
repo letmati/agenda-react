f6db7046e810fe18ef13cb9517dbf89c1f1a89f4
letmati <sdevmatt@gmail.com>
2024-04-24 17:21:15 -0300

    fix portada bg position, add opacity transition

--- src/components/Portada/index.jsx
+++ src/components/Portada/index.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "/assets/logo-portada.png";
 import portada from "/assets/portada.jpg";
 
 export default function Portada() {
+  const [loaded, setLoaded] = useState()
+
   return (
     <section
       className="flex justify-center items-center"
       style={{
         backgroundImage: `url(${portada})`,
         backgroundSize: "cover",
+        backgroundPosition: "center",
         height: "100svh",
       }}
     >
       <div className="fade-in-portada flex justify-center">
-        <img className="lg:w-1/4 w-1/2" src={logo} alt="Logo"  />
+        <img onLoad={() => setLoaded(true)} className={`lg:w-1/4 w-4/5 transition-opacity duration-[4000ms] ${loaded ? 'opacity-1' : 'opacity-0'}`} src={logo} alt="Logo"  />
       </div>
     </section>
   );
